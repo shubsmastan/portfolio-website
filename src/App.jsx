70a9482565c3babe48ac17f38ff0198dd99a3351
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-04-22 20:20:19 +0100

    Use Tailwind CSS to style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +27,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <FontAwesomeIcon icon={faGithub} />
     </>
   );
 }
