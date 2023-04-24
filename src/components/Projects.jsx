aa756b8259e39753edbf02ad73d60944fe73c843
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-04-24 01:18:12 +0100

    Change responsiveness in portfolio projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,13 +19,14 @@ export default function Projects(props) {
         onClick={props.handleClickNext}
         className="absolute right-[10px] top-[260px] sm:right-[70px] md:right-[90px] lg:right-[120px] cursor-pointer focus:text-dark-rose hover:text-dark-rose active:text-2xl z-20 origin-center"
       />
+      <div className="flex flex-col mx-auto w-100 h-[450px] sm:w-2/3"></div>
       {props.projects.map((project, index) => {
         return (
           <div
             className={
               index === props.sliderIndex
-                ? "flex flex-col mx-auto w-100 sm:w-2/3 transition-opacity duration-1000 ease-in-out"
-                : "flex flex-col mx-auto w-100 absolute sm:w-2/3 opacity-0"
+                ? "flex flex-col mx-auto absolute w-100 sm:left-[100px] md:left-[125px] lg:left-[160px] 2xl:left-[200px] sm:w-2/3 transition-opacity duration-1000 ease-in-out z-1"
+                : "flex flex-col mx-auto absolute w-100 sm:left-[100px] md:left-[125px] lg:left-[160px] 2xl:left-[200px] sm:w-2/3 opacity-0 transition-opacity duration-1000 ease-in-out z-0"
             }
             key={project.title}
           >
@@ -34,7 +35,7 @@ export default function Projects(props) {
               className="w-full h-[250px] object-cover object-center rounded-t"
               src={project.image}
             />
-            <div className="px-8 py-5 h-[200px] w-full bg-cream rounded-b">
+            <div className="px-8 py-5 h-[250px] sm:h-[200px] w-full bg-cream rounded-b">
               <h2 className="text-base font-bold mb-1">{project.subtitle}</h2>
               <h1 className="text-lg text-dark-rose font-bold font-large mb-3">
                 <a href={project.link} target="_blank">
@@ -46,7 +47,7 @@ export default function Projects(props) {
           </div>
         );
       })}
-      <div className="absolute flex gap-2 right-1/2 translate-x-1/2 -bottom-[30px]">
+      <div className="absolute flex gap-2 right-1/2 translate-x-1/2 -bottom-[80px] sm:-bottom-[30px]">
         {props.projects.map((project, index) => {
           return (
             <FontAwesomeIcon
